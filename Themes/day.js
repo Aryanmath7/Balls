37ed910b5f350b33cb8907678bad8e02f2c2ba71
Aryan Mathur <Aryanmath7@gmail.com>
2025-06-14 00:20:16 -0500

    Adding in basic skybox elements

--- Themes/day.js
+++ Themes/day.js
@@ -2,3 +2,4 @@
 export const LIGHT_COLOR = 0xffffff; // white light
 export const LIGHT_INTENSITY = 0.8; // intensity of the light
 export const LIGHT_DIRECTION = { x: 5, y: 10, z: 7 }; // position of the light
+export const LIGHT_SKYBOX_PATH = './textures/skybox/daytime/';
